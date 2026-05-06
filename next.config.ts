794cb9d2387a2b055c216e41be75512cba5b8b81
DevDAJ <danial.aiman@devdaj.tech>
2026-05-06 09:23:03 +0800

    feat: make this application a PWA

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,49 @@
 import type { NextConfig } from "next";
 
+/**
+ * PWA-related headers from
+ * https://nextjs.org/docs/app/guides/progressive-web-apps#8-securing-your-application
+ */
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/javascript; charset=utf-8",
+          },
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
